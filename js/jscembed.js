10ad8b6b713278353e867f0c7179e01fa6d98471
MathWizz <mathwizz94@gmail.com>
2012-10-23 22:17:12 -0500

    updated embedding script

--- js/jscembed.js
+++ js/jscembed.js
@@ -1,10 +1,20 @@
-document.addEventListener('DOMContentLoaded', function () {
-	var divs = document.querySelectorAll('div[project]');
-	var i = divs.length;
-	while (i--) {
-	var project = jsc.createPlayer(divs[i].getAttribute('project'), false);
-		window.player = project[1];
-		divs[i].innerHTML = '';
-		divs[i].appendChild(project[0]);
+(function () {
+	var done = [];
+	
+	function update() {
+		var divs = document.querySelectorAll('div[project]');
+		var i = divs.length;
+		while (i--) {
+			if (done.indexOf(divs[i]) === -1) {
+				done.push(divs[i]);
+				var project = jsc.createPlayer(divs[i].getAttribute('project'), divs[i].getAttribute('autoplay') === 'true');
+				window.player = project[1];
+				divs[i].innerHTML = '';
+				divs[i].appendChild(project[0]);
+			}
+		}
 	}
-});
+
+	document.addEventListener('DOMContentLoaded', update, false);
+	document.addEventListener("DOMNodeInserted", update, false);
+}) ();
