5f9a86b958b113d5bf6ccccc8538758cd77ce9e9
MathWizz <mathwizz94@gmail.com>
2013-05-14 20:13:29 -0500

    Removed compressor

--- bin/compress.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-cd %~dp0/../js
-copy /b datatypes.js+jsscratch.js+scratchio.js+jdataview.js+blocks.js tmp.js
-java -jar %~dp0/yuicompressor-2.4.7.jar tmp.js -o ../jsscratch.min.js
-del tmp.js
-cd ..
-java -jar %~dp0/yuicompressor-2.4.7.jar player.css -o player.min.css
-pause
